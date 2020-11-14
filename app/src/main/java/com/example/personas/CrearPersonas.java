a65e58cd0e29486a37791aea97e20d03ca3c5b8d
Carlos Cabas <ing.carloscabas@gmail.com>
2020-11-14 16:41:05 -0500

    almacenamiento funcional

--- app/src/main/java/com/example/personas/CrearPersonas.java
+++ app/src/main/java/com/example/personas/CrearPersonas.java
@@ -3,6 +3,8 @@ package com.example.personas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CrearPersonas extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class CrearPersonas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_personas);
+
+
+
     }
+
 }
--- app/src/main/java/com/example/personas/ListarPersonas.java
+++ app/src/main/java/com/example/personas/ListarPersonas.java
@@ -3,12 +3,45 @@ package com.example.personas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ListarPersonas extends AppCompatActivity {
+
+    private EditText cedula, nombre, apellido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_personas);
+
+        cedula = findViewById(R.id.txtCedula);
+        nombre = findViewById(R.id.txtNombre);
+        apellido = findViewById(R.id.txtApellido);
     }
+
+    public void guardar(View v){
+        String ced, nom, apell;
+        Persona p;
+        ced = cedula.getText().toString();
+        nom = nombre.getText().toString();
+        apell = apellido.getText().toString();
+        p = new Persona(ced, nom, apell);
+        p.guardar();
+        Toast.makeText(this,"Persona guardada Exitosamente",Toast.LENGTH_LONG).show();
+        limpiar();
+    }
+
+    public void limpiar(View v){
+        limpiar();
+    }
+
+    public void limpiar(){
+        cedula.setText("");
+        nombre.setText("");
+        apellido.setText("");
+        cedula.requestFocus();
+    }
+
 }
